422d97ca67a8e9f983e321d29e38e35a386e2d72
amangupt11 <amangupta2807560@gmail.com>
2025-04-09 22:19:19 +0530

    change

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,10 +13,11 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
